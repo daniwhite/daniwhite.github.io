f94fb970513a2e32894d8bf2b3a41a0ba257561e
Dani White <white.dani.m@gmail.com>
2017-07-11 18:45:04 -0700

    Restyle, add favicon, fix js

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
+var navTop;
+$(document).ready(function() {
+    navTop = $('#nav-static').position().top;
+})
+
 $(window).scroll(function(){
     var scrollTop = $(window).scrollTop();
-    var top = $('#nav').position().top;
-    var offset = window.innerHeight*.2;
-    if(scrollTop < offset){
-          $('#nav').css('top',offset-scrollTop);
+    var navTop = $('#nav-static').position().top;
+    console.log("Scrolltop: " + scrollTop);
+    console.log("NavTop: " + navTop);
+    console.log("Current NavTop: " + $('#nav-static').position().top);
+    console.log("Difference: " + (navTop-scrollTop));
+    if(scrollTop < navTop){
+          $('#nav-fixed').css('visibility','hidden');
+          $('#nav-static').css('visibility','visible');
     } else {
-        $('#nav').css('top','0');
+        $('#nav-fixed').css('visibility','visible');
+        $('#nav-static').css('visibility','hidden');
     }
 });
